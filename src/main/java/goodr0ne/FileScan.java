1b96cbe414de988d4d77a643da918e7ae0a8b9bf
Yegor Scherbatkin <goodronechosen@gmail.com>
2018-11-03 20:08:21 +0300

    Add picocli launcher and correct jar packaging

--- src/main/java/goodr0ne/FileScan.java
+++ src/main/java/goodr0ne/FileScan.java
@@ -1,7 +1,25 @@
 package goodr0ne;
 
-public class FileScan {
-  public static void main(String[] args) {
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "filescan", mixinStandardHelpOptions = true, version = "goodr0ne.filescan 0.47")
+public class FileScan implements Runnable {
+
+  @CommandLine.Option(names = "-readDrives" , description = "Outputs all available drives list.")
+  private static boolean isReadDrives = false;
+
+  private static void readDrives() {
+    System.out.println("-readDrives");
+  }
+
+  public void run() {
     System.out.println("Hello World!");
+    if (isReadDrives) {
+      readDrives();
+    }
+  }
+
+  public static void main(String[] args) {
+    CommandLine.run(new FileScan(), args);
   }
 }
