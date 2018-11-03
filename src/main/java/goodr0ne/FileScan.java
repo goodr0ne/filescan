23a1c40515ce6bf205a1fc452a7c8c21ad5623f3
Yegor Scherbatkin <goodronechosen@gmail.com>
2018-11-04 02:08:48 +0300

    add exact file search in all drives feature

--- src/main/java/goodr0ne/FileScan.java
+++ src/main/java/goodr0ne/FileScan.java
@@ -5,6 +5,8 @@ import picocli.CommandLine;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Objects;
 
 @CommandLine.Command(name = "filescan",
         mixinStandardHelpOptions = true, version = "goodr0ne.filescan 1.337")
@@ -23,7 +25,11 @@ public class FileScan implements Runnable {
           description = "Add specified string to searched file (first found one)")
   private static String appendLineArg = "";
 
+  private static long fileCount = 0;
+  private static long recursiveCount = 0;
   private static File foundFile;
+  private static ArrayList<File> foundFiles = new ArrayList<>();
+  private static boolean[] isOutputted = {false, false, false, false};
 
   private static String readFile(File file, boolean isPreview) {
     StringBuilder output = new StringBuilder();
@@ -48,28 +54,77 @@ public class FileScan implements Runnable {
     FileSystemView fsv = FileSystemView.getFileSystemView();
     File[] paths = File.listRoots();
     for (File path:paths) {
-      System.out.println("Drive Name: "+path);
-      System.out.println("Description: "+fsv.getSystemTypeDescription(path));
+      System.out.println("Drive Name: " + path);
+      System.out.println("Description: " + fsv.getSystemTypeDescription(path));
     }
     System.out.println();
+  }
+
+  private static long fileCount(File dir) {
+    try {
+      File[] files = dir.listFiles();
+      int count = 0;
+      assert files != null;
+      for (File f : files)
+        if (f.isDirectory())
+          count += fileCount(f);
+        else
+          count++;
+      return count;
+    } catch (Exception e) {
+      return 0;
+    }
   }
 
   private static void previewFoundFiles(File[] files) {
     int count = 1;
     for (File file:files) {
       if (count == 1) {
-        System.out.println("\n" + count + "st Found file - " + file.getName());
+        System.out.println("\n" + count + "st Found file - " + file.getAbsolutePath());
       } else if (count == 2) {
-        System.out.println("\n" + count + "nd Found file - " + file.getName());
+        System.out.println("\n" + count + "nd Found file - " + file.getAbsolutePath());
       } else if (count == 3) {
-        System.out.println("\n" + count + "rd Found file - " + file.getName());
+        System.out.println("\n" + count + "rd Found file - " + file.getAbsolutePath());
       } else {
-        System.out.println("\n" + count + "th Found file - " + file.getName());
+        System.out.println("\n" + count + "th Found file - " + file.getAbsolutePath());
       }
       System.out.println("File content preview (first 7 lines):");
       System.out.println(">>>FILE_BEGINS_HERE>>>" + readFile(file, true) + "<<<FILE_ENDS_HERE<<<");
       count++;
     }
+  }
+
+  private static void searchRecursively(File file) {
+    recursiveCount++;
+    if (!isOutputted[0] && (recursiveCount > (fileCount / 5))) {
+      isOutputted[0] = true;
+      System.out.println("Processed 20% of all files");
+    } else if (!isOutputted[1] && (recursiveCount > 2 * (fileCount / 5))) {
+      isOutputted[1] = true;
+      System.out.println("Processed 40% of all files");
+    } else if (!isOutputted[2] && (recursiveCount > 3 * (fileCount / 5))) {
+      isOutputted[2] = true;
+      System.out.println("Processed 60% of all files");
+    } else if (!isOutputted[3] && (recursiveCount > 4 * (fileCount / 5))) {
+      isOutputted[3] = true;
+      System.out.println("Processed 80% of all files");
+    }
+    try {
+      if (file.isDirectory()) {
+        if (file.canRead()) {
+          for (File temp : Objects.requireNonNull(file.listFiles())) {
+            if (temp.isDirectory()) {
+              searchRecursively(temp);
+            } else {
+              if (searchFileArgs[0].equals(temp.getName())) {
+                System.out.println("File found! Path - " + temp.getAbsolutePath());
+                foundFiles.add(temp);
+              }
+            }
+          }
+        }
+      }
+    } catch (Exception ignored) {}
   }
 
   private static int searchFile() {
@@ -81,11 +136,8 @@ public class FileScan implements Runnable {
       System.out.println("Searching filename - " + searchFileArgs[1]);
       try {
         File path = new File(searchFileArgs[0]);
-        File[] matchingFiles = path.listFiles(new FilenameFilter() {
-          public boolean accept(File dir, String name) {
-            return name.contains(searchFileArgs[1]);
-          }
-        });
+        File[] matchingFiles = path.listFiles((dir, name) ->
+                name.toLowerCase().contains(searchFileArgs[1].toLowerCase()));
         if ((matchingFiles != null) && (matchingFiles.length > 0)) {
           System.out.println("File(s) found! Total quantity - " + matchingFiles.length);
           foundFile = matchingFiles[0];
@@ -101,8 +153,28 @@ public class FileScan implements Runnable {
     } else {
       System.out.println("Launching exact filename match search in all directories");
       System.out.println("Searching filename - " + searchFileArgs[0]);
-
-      status = 0;
+      File[] drives = File.listRoots();
+      System.out.println("Producing all files recount operation");
+      for (File drive:drives) {
+        long count = fileCount(new File(drive + "\\"));
+        System.out.println("Drive " + drive + " files count - " + count);
+        fileCount = fileCount + count;
+      }
+      System.out.println("Total files count - " + fileCount);
+      for (File drive:drives) {
+        System.out.println("Searching in drive " + drive);
+        long time = System.currentTimeMillis();
+        searchRecursively(new File(drive + "\\"));
+        System.out.println("Drive search time - "
+                + ((System.currentTimeMillis() - time) / 1000) + " seconds");
+      }
+      if (foundFiles.size() > 0) {
+        System.out.println(foundFiles.size() + " files are found!");
+        previewFoundFiles(foundFiles.toArray(new File[0]));
+      } else {
+        System.out.println("No files are found!");
+      }
+      status = 1;
     }
     System.out.println();
     return status;
@@ -150,6 +222,7 @@ public class FileScan implements Runnable {
 
   public void run() {
     System.out.println("\nFileScan is launched, fasten your seat belt!\n");
+    long time = System.currentTimeMillis();
     if (isReadDrives) {
       readDrives();
     }
@@ -159,6 +232,8 @@ public class FileScan implements Runnable {
         appendLine();
       }
     }
+    System.out.println("Total spent time - "
+            + ((System.currentTimeMillis() - time) / 1000) + " seconds");
     System.out.println("Thanks for using, enjoy your day!");
   }
 
